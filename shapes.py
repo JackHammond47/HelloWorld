2260a215144ea26491c7c5c1f50150e2d6c08f9b
JackHammond47 <jackson.hammond47@gmail.com>
2024-05-08 11:57:47 -0600

    added shapes.py

---
+++ shapes.py
@@ -0,0 +1,8 @@
+def calculate_rect_area(base, height):
+    return base * height
+
+def main():
+    rectangle_area = calculate_rect_area(10,2)
+    print(f'The area of the rectangle is {rectangle_area}.')
+
+main()

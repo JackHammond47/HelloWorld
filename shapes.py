558ef43dda8d3506799472ee7323cc1673bd5617
JackHammond47 <jackson.hammond47@gmail.com>
2024-05-08 12:19:38 -0600

    merged branch

--- shapes.py
+++ shapes.py
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-def main():
-    print("The area of the rectangle is 20.")
-=======
 def calculate_rect_area(base, height):
     return base * height
 
 def main():
     rectangle_area = calculate_rect_area(10,2)
     print(f'The area of the rectangle is {rectangle_area}.')
->>>>>>> shapes1
 
 main()
